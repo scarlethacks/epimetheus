3683c4ab145b375da5dedb58a12e6734df8d68e7
Vinesh Kannan <vkannan3@hawk.iit.edu>
2017-02-25 02:20:05 -0600

    Add firebase

--- bot.js
+++ bot.js
@@ -1,18 +1,35 @@
 var Bot = require('slackbots');
+var firebase = require('firebase');
 var dotenv = require('dotenv');
-    dotenv.load();
+	dotenv.load();
+
+var config = {
+	apiKey: "AIzaSyBXXQFcl6qtakmkFeh0jzy_jjjIDpb1DlY",
+	authDomain: "prometheusjs.firebaseapp.com",
+	databaseURL: "https://prometheusjs.firebaseio.com",
+	storageBucket: "firebase-prometheusjs.appspot.com",
+	messagingSenderId: "433905102741"
+};
+firebase.initializeApp(config);
+var db = firebase.database();
+
+var ref = db.ref('prometheus/users/jenny');
+ref.once('value', (snapshot) => {
+	var val = snapshot.val();
+	console.log(val);
+});
 
 var slackBotToken = process.env.SLACK_BOT_TOKEN || '';
 var slackBotName  = process.env.SLACK_BOT_NAME  || '';
 
 var bot = new Bot({
-    token: slackBotToken,
-    name : slackBotName
+	token: slackBotToken,
+	name : slackBotName
 });
 bot.knownUsers = {};
 
 bot.on('start', function() {
-    console.log('[BOT] Started');
+	console.log('[BOT] Started');
 });
 
 bot.on('message', function(data) {
